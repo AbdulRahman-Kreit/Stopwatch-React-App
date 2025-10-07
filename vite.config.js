92f0027067ff7a310abeec077891c209fe704e03
AbdulRahman-Kreit <aleppianeren@gmail.com>
2025-10-07 11:14:36 +0300

    Fix the env variable

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import { process } from 'node:process'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/stopwatch-react-app/',
+  base: process.env.VITE_BASE_PATH || '/stopwatch-react-app/',
 })
